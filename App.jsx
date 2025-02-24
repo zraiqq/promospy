86eb41abd276c0e161e58a96b458defb9be40049
zraiqq <enzo.derrien2222@gmail.com>
2025-02-24 21:00:15 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PriceTracker from "./PriceTracker";
+
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold mb-4">Promospy</h1>
+      <PriceTracker />
+    </div>
+  );
+}
+
+export default App;
