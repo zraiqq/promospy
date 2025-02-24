3f076dad88e4e938d467e973d870fcf169b149ff
zraiqq <enzo.derrien2222@gmail.com>
2025-02-24 21:57:35 +0100

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import PriceTracker from "./pricetracker";
+
+function App() {
+  return (
+    <div>
+      <h1>PromoSpy - Comparatif de prix</h1>
+      <PriceTracker />
+    </div>
+  );
+}
+
+export default App;
